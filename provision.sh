233e0c09a40b30414692bfa33989fe1672108155
RobertHa <robert.hamsch@tum.de>
2018-04-23 13:25:01 +0200

    trying something out

--- provision.sh
+++ provision.sh
@@ -9,16 +9,21 @@
 # Contributor: Bernhard Blieninger
 ######################
 sudo apt-get update -qq
-sudo apt-get install python3.5 -qq #probably needs sudo
-sudo apt-get  install python3-pip -qq
-sudo apt-get install bridge-utils -qq
-#sudo pip3 install service #should be pip3, do same commandswork? https://pypi.python.org/pypi/service/0.4.1
-#sudo pip3 install pynetlinux #1.1 or somewhat close with the 1.1 and everything https://pypi.python.org/pypi/pynetlinux/1.1
+sudo apt-get install python3.5 python3-pip bridge-utils screen nmap isc-dhcp-server qemu htop pkg-config zlib1g-dev libglib2.0 libpixman-1.0 libpixman-1-dev -qq #probably needs sudo
+#sudo apt-get  install python3-pip -qq
+#mkdir -p qemu
+#cd qemu
+#wget -qO- https://download.qemu.org/qemu-2.11.1.tar.xz | tar xJ
+#cd qemu-2.11.1
+#./configure --target-list=arm-softmmu
+#make -j4
+#sudo make install
+#sudo apt-get install bridge-utils -qq
 sudo pip3 install -r /vagrant/taskgen/requirements.txt
-#sudo sh -c 'echo "import sys\nsys.path.append(\"/usr/local/lib/python3.5/dist-packages/pynetlinux\")\n$(cat /usr/local/lib/python3.5/dist-packages/pynetlinux/__init__.py)" > /usr/local/lib/python3.5/dist-packages/pynetlinux/__init__.py'
+sudo pip3 install dicttoxml
 sudo sh -c 'echo "auto br0\niface br0 inet dhcp\nbridge_ports eth0\nbridge_stp off\nbridge_maxwait 0\nbridge_fd 0\n" >> /etc/network/interfaces'
-sudo apt-get install qemu -qq
-sudo apt-get install screen -qq
-sudo apt-get install nmap isc-dhcp-server -qq
+#sudo apt-get install qemu -qq
+#sudo apt-get install screen -qq
+#sudo apt-get install nmap isc-dhcp-server -qq
 sudo cp /vagrant/dhcpd.conf /etc/dhcp/
-sudo apt-get install htop -qq
+#sudo apt-get install htop -qq
