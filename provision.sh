04fd1d792aa5efd1d24db6e31cfa34e81c56b1c4
RobertHa <robert.hamsch@tum.de>
2018-07-30 15:35:26 +0200

    added contributor

--- provision.sh
+++ provision.sh
@@ -6,7 +6,7 @@
 # If you have commands that you want to run always please have a
 # look at the bootstrap.sh script
 #
-# Contributor: Bernhard Blieninger
+# Contributor: Bernhard Blieninger, Robert Hamsch
 ######################
 
 # compile qemu
