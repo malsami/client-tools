9342d4db1c5ae029c11f57184300222a67aa1a85
Robert <robert.hamsch@tum.de>
2018-08-13 14:06:34 +0200

    Working (#6)
    * added to provision.sh
    
    * added missing dhcp config file
    
    * bugfixes but right now sudo systemctl start isc-dhcp-server needs to be executed after vagrant up to enable the dhcp
    
    * removed unnecessary modules
    
    * trying something out
    
    * now the dhcp works right away
    
    * addded pip upgrade
    
    * added additional mappings to the dhcpd.conf and removed vagrant paths from the provision script
    
    * provision.sh works now with and without vagrant
    
    * now the correct version of qemu is installed(2.12.0)
    
    * Edit of network ranges and ips
    
    * Removed unecessary stuff and changed network range
    
    * Updated Provision 1/2
    
    * added contributor
    
    * Update twice assigned ip address 2/2
    
    * changed gateway .10 to .254

--- provision.sh
+++ provision.sh
@@ -6,13 +6,33 @@
 # If you have commands that you want to run always please have a
 # look at the bootstrap.sh script
 #
-# Contributor: Bernhard Blieninger
+# Contributor: Bernhard Blieninger, Robert Hamsch
 ######################
+
+# compile qemu
+QEMU_VERSION=2.12.0
+mkdir -p $HOME/Downloads/QEMU
+cd $HOME/Downloads/QEMU
+wget https://download.qemu.org/qemu-$QEMU_VERSION.tar.xz
+tar xvJf qemu-$QEMU_VERSION.tar.xz
+cd qemu-$QEMU_VERSION
+./configure --target-list=arm-softmmu
+make -j
+sudo cp arm-softmmu/qemu-system-arm /usr/local/bin/
+
+if [ $USER == "ubuntu" ] || [ $USER == "vagrant" ]; then
+  cd /vagrant
+fi
 sudo apt-get update -qq
-sudo apt-get install python3.5 -qq #probably needs sudo
-sudo apt-get  install python3-pip -qq
-sudo apt-get install bridge-utils -qq
-sudo pip3 install service #should be pip3, do same commandswork? https://pypi.python.org/pypi/service/0.4.1
-sudo pip3 install pynetlinux #1.1 or somewhat close with the 1.1 and everything https://pypi.python.org/pypi/pynetlinux/1.1
-sudo pip3 install -r /vagrant/taskgen/requirements.txt
-sudo sh -c 'echo "import sys\nsys.path.append(\"/usr/local/lib/python3.5/dist-packages/pynetlinux\")\n$(cat /usr/local/lib/python3.5/dist-packages/pynetlinux/__init__.py)" > /usr/local/lib/python3.5/dist-packages/pynetlinux/__init__.py'
+sudo apt-get install python3.5 python3-pip bridge-utils screen isc-dhcp-server htop pkg-config zlib1g-dev libglib2.0 libpixman-1.0 libpixman-1-dev -qq #probably needs sudo
+sudo pip3 install --upgrade pip
+sudo pip3 install -r ./taskgen/requirements.txt
+sudo pip3 install dicttoxml
+sudo sh -c 'echo "\nauto br0\niface br0 inet static\n\thwaddress ether DE:AD:BE:EF:69:01\n\taddress 10.200.45.254\n\tnetmask 255.255.255.0\n\tgateway 10.200.45.254\nbridge_stp off\nbridge_maxwait 0\nbridge_fd 0\n" >> /etc/network/interfaces'
+
+sudo /etc/init.d/networking restart
+
+sudo cp ./dhcpd.conf /etc/dhcp/
+
+sudo systemctl enable isc-dhcp-server
+sudo systemctl start isc-dhcp-server
