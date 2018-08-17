1980db51540cab7e589258fefbde7f33a50a02a1
RobertHa <robert.hamsch@tum.de>
2018-08-17 13:04:12 +0200

    fixed issues due to ubuntu and the defined subnet

--- provision.sh
+++ provision.sh
@@ -9,30 +9,31 @@
 # Contributor: Bernhard Blieninger, Robert Hamsch
 ######################
 
+sudo apt-get update -qq 
+sudo apt-get install python3.5 python3-pip bridge-utils screen isc-dhcp-server htop pkg-config zlib1g-dev libglib2.0 libpixman-1.0 libpixman-1-dev -qq 
+sudo pip3 install --upgrade pip 
+sudo pip3 install dicttoxml 
+sudo sh -c 'echo "\nauto br0\niface br0 inet static\n\thwaddress ether DE:AD:BE:EF:69:01\n\taddress 10.200.45.254\n\tnetmask 255.255.255.0\n\tgateway 10.200.45.254\nbridge_ports eth0\nbridge_stp off\nbridge_maxwait 0\nbridge_fd 0\n" >> /etc/network/interfaces'
+
 # compile qemu
 QEMU_VERSION=2.12.0
 mkdir -p $HOME/Downloads/QEMU
 cd $HOME/Downloads/QEMU
-wget https://download.qemu.org/qemu-$QEMU_VERSION.tar.xz
+wget -q https://download.qemu.org/qemu-$QEMU_VERSION.tar.xz
 tar xvJf qemu-$QEMU_VERSION.tar.xz
 cd qemu-$QEMU_VERSION
 ./configure --target-list=arm-softmmu
-make -j
+make
 sudo cp arm-softmmu/qemu-system-arm /usr/local/bin/
+# compile qemu finished
 
 if [ $USER == "ubuntu" ] || [ $USER == "vagrant" ]; then
   cd /vagrant
 fi
-sudo apt-get update -qq
-sudo apt-get install python3.5 python3-pip bridge-utils screen isc-dhcp-server htop pkg-config zlib1g-dev libglib2.0 libpixman-1.0 libpixman-1-dev -qq #probably needs sudo
-sudo pip3 install --upgrade pip
-sudo pip3 install -r ./taskgen/requirements.txt
-sudo pip3 install dicttoxml
-sudo sh -c 'echo "\nauto br0\niface br0 inet static\n\thwaddress ether DE:AD:BE:EF:69:01\n\taddress 10.200.45.254\n\tnetmask 255.255.255.0\n\tgateway 10.200.45.254\nbridge_stp off\nbridge_maxwait 0\nbridge_fd 0\n" >> /etc/network/interfaces'
+sudo pip3 install -r ./taskgen/requirements.txt > /dev/null
+sudo cp ./dhcpd.conf /etc/dhcp/
 
 sudo /etc/init.d/networking restart
-
-sudo cp ./dhcpd.conf /etc/dhcp/
 
 sudo systemctl enable isc-dhcp-server
 sudo systemctl start isc-dhcp-server
