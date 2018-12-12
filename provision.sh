f9cfb6a5b1c652e888c27f6d14b341de8fe99089
RobertHa <robert.hamsch@tum.de>
2018-12-12 15:30:58 +0100

    changed requirement files

--- provision.sh
+++ provision.sh
@@ -12,7 +12,7 @@
 sudo apt-get update -qq 
 sudo apt-get install python3.5 python3-pip bridge-utils screen isc-dhcp-server htop pkg-config zlib1g-dev libglib2.0 libpixman-1.0 libpixman-1-dev -qq 
 sudo pip3 install --upgrade pip 
-sudo pip3 install dicttoxml 
+sudo pip3 install -r ./distributor_service/requirements.txt > /dev/null 
 sudo sh -c 'echo "\nauto br0\niface br0 inet static\n\thwaddress ether DE:AD:BE:EF:69:01\n\taddress 10.200.45.254\n\tnetmask 255.255.255.0\n\tgateway 10.200.45.254\nbridge_ports eth0\nbridge_stp off\nbridge_maxwait 0\nbridge_fd 0\n" >> /etc/network/interfaces'
 
 # compile qemu
@@ -30,7 +30,6 @@ sudo cp arm-softmmu/qemu-system-arm /usr/local/bin/
 if [ $USER == "ubuntu" ] || [ $USER == "vagrant" ]; then
   cd /vagrant
 fi
-sudo pip3 install -r ./taskgen/requirements.txt > /dev/null
 sudo cp ./dhcpd.conf /etc/dhcp/
 
 sudo /etc/init.d/networking restart
