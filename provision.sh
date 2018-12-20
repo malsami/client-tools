edde66e6b9fd6670e05edd5b7f7cee51ccc2064c
RobertHa <robert.hamsch@tum.de>
2018-12-20 18:43:54 +0100

    adapted provision

--- provision.sh
+++ provision.sh
@@ -11,8 +11,7 @@
 
 sudo apt-get update -qq 
 sudo apt-get install python3.5 python3-pip bridge-utils screen isc-dhcp-server htop pkg-config zlib1g-dev libglib2.0 libpixman-1.0 libpixman-1-dev -qq 
-sudo pip3 install --upgrade pip 
-sudo pip3 install -r ./distributor_service/requirements.txt > /dev/null 
+
 sudo sh -c 'echo "\nauto br0\niface br0 inet static\n\thwaddress ether DE:AD:BE:EF:69:01\n\taddress 10.200.45.254\n\tnetmask 255.255.255.0\n\tgateway 10.200.45.254\nbridge_ports eth0\nbridge_stp off\nbridge_maxwait 0\nbridge_fd 0\n" >> /etc/network/interfaces'
 
 # compile qemu
@@ -29,10 +28,15 @@ sudo cp arm-softmmu/qemu-system-arm /usr/local/bin/
 
 if [ $USER == "ubuntu" ] || [ $USER == "vagrant" ]; then
   cd /vagrant
+  pip3 install --upgrade pip
+  pip3 install -r ./datageneration/requirements.txt > /dev/null 
+  pip3 install -r ./distributor/requirements.txt > /dev/null 
+else
+
 fi
 sudo cp ./dhcpd.conf /etc/dhcp/
-
 sudo /etc/init.d/networking restart
 
 sudo systemctl enable isc-dhcp-server
 sudo systemctl start isc-dhcp-server
+
